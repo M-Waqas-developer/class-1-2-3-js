c2daea15f9fc973f740ff4bb363602c8f0f55c96
M-Waqas-developer <waqasmuhammad1209@gmail.com>
2024-08-16 16:18:04 +0500

    Add files via upload

---
+++ first.js
@@ -0,0 +1,19 @@
+alert("Hello World");
+
+var name1="Muhammad Waqas";
+alert(name1);
+var age="19";
+alert(age);
+var message="Web and App Development";
+alert(message);
+var email=("mY EMAIL is examplecode@gmail.com");
+alert(email);
+
+var book=("A smarter way to learn javascript");
+alert(book);
+
+var design=("▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬");
+alert(design);
+
+let contentDiv = document.getElementById('content');
+contentDiv.innerHTML = '<h1>Hello, World!</h1><p>This content was written through JavaScript.</p>';
---
+++ index2.js
@@ -0,0 +1,7 @@
+// script.js
+
+// Select the 'content' div by its ID
+let contentDiv = document.getElementById('content');
+
+// Replace the content with new HTML
+contentDiv.innerHTML = '<h1>Hello, World!</h1><p>This content was written through JavaScript.</p><p>Visitor’s name: Waqas</p><p>Product Title: Jeans</p><p>Quantity: Total Quantity is 5</p><p><b>Waqas</b> ordered <b>5 Jeans</b> on XYZ Clothing store';
